1c0bec75ee1fca843fc494f61163649baa3d7e62
laurelcrelia <laurel.matt@helsinki.fi>
2024-06-27 21:05:44 +0300

    Remove unnecessary commented code

--- index.js
+++ index.js
@@ -44,18 +44,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 // Create a new person
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  // if (!body.name) {
-  //   return response.status(400).json({
-  //     error: "name missing",
-  //   });
-  // }
-  // if (!body.number) {
-  //   return response.status(400).json({
-  //     error: "number missing",
-  //   });
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
